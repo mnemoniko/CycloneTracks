6f4d4d3f62ff016db88baf7a06d3e1178d0f9d8d
mnemoniko <mnemoniko@users.noreply.github.com>
2020-03-04 11:23:45 +0000

    Put user defined values in separate file

---
+++ user_parameters.jl
@@ -0,0 +1,26 @@
+#User-set parameters for contour-based cyclone/sea ice tracking
+
+#Directory containing ERA5 files
+era5_dir = "./ERA5"
+#NetCDF file to save processed data
+outfile = "ERA5_2018_2019_msl.nc"
+
+#Start time:
+Year_S = 2019
+Month_S = 1
+Day_S = 1
+Hour_S = 0
+
+#End time:
+Year_E = 2019
+Month_E = 11
+Day_E = 1
+Hour_E = 0
+
+#Latitude:
+lat_min = -88
+lat_max = -60
+
+#Longitude (0 to 360):
+lon_min = 160
+lon_max = 220
